f394d7b40417ca3ffea63b3a4aeb054719f930fb
Sandra Babović <sandra.babovic.1994@gmail.com>
2019-02-11 14:51:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
         // Transition effect for navbar 
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
+        $(window).scroll(function() {s
+          // checks if window is scrolled more than 0px, changes background color
           if($(this).scrollTop() > 0) { 
               $('#navbar').css({ "background-color" : "rgba(0, 0, 0, 1)"});
           } else {
